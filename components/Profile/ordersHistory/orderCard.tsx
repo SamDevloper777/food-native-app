e8e09027874a7d7f54fae493d089f7943443932f
Shivam Verma <mcrafter147@gmail.com>
2025-04-07 22:46:14 +0530

    [SCREEN]: Orders History

--- components/Profile/ordersHistory/orderCard.tsx
+++ components/Profile/ordersHistory/orderCard.tsx
@@ -1,28 +1,24 @@
-import { Ionicons, Entypo } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
 import React from 'react'
 import { Image, SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
 
-const WishlistCard = ({
+const OrderCard = ({
     title,
     description,
     rating,
-    city,
-    cost,
-    distance,
     time,
-    url
+    cost,
+    url,
 }: {
     title: string
     description: string
     rating: number
-    city: string
-    cost: number
-    distance: number
     time: string
+    cost: number
     url: string
 }) => {
     return (
-        <SafeAreaView className='px-6 mt-4 mb-2'>
+        <SafeAreaView className='px-6 mt-2 mb-4'>
             <View className='h-48 w-full rounded-[25px] relative shadow-lg shadow-gray-700'>
                 <Image source={{ uri: url }} className='w-full h-full rounded-[25px]' />
                 <View className='absolute z-10 top-0 right-0 w-[60%] h-full bg-white rounded-r-[25px]'>
@@ -35,33 +31,26 @@ const WishlistCard = ({
                             <Text className='text-sm font-bold text-[#2C8D2C] mt-1'>
                                 ⭐ {rating} (300k+ Reviews)
                             </Text>
-                            <View className='w-full h-[1px] bg-gray-200 mt-8' />
+                            <View className='w-full h-[1px] bg-gray-200 mt-5' />
                         </View>
                         <View className='flex flex-row justify-between items-center'>
                             <View className='flex flex-col justify-between h-12'>
-                                <Text className='text-sm text-black'>{city}</Text>
                                 <Text className='text-[#FC913A] font-bold text-base'>${cost} for one</Text>
-                            </View>
-                            <View className='w-[1px] h-12 bg-gray-200' />
-                            <View className='flex flex-col justify-between h-12'>
-                                <View className='flex flex-row items-center space-x-1'>
-                                    <Entypo name='location-pin' size={16} color='#FC913A' />
-                                    <Text className='text-sm'>{distance} km</Text>
-                                </View>
-                                <View className='flex flex-row items-center space-x-1'>
-                                    <Ionicons name='time-outline' size={16} color='gray' />
-                                    <Text className='text-sm'>{time}</Text>
+                                <View className='flex flex-row items-center space-x-1 gap-1'>
+                                    <Ionicons name='time-outline' size={16} color='gray-200' />
+                                    <Text className='text-xs text-gray-700'>{time}</Text>
                                 </View>
                             </View>
+                            <View className='w-[1px] h-12 bg-gray-200 mx-4' />
+                            <TouchableOpacity className='bg-[#FC913A] px-4 py-1.5 rounded-full self-center' activeOpacity={0.8}>
+                                <Text className='text-white text-md font-semibold'>Reorder</Text>
+                            </TouchableOpacity>
                         </View>
                     </View>
                 </View>
-                <TouchableOpacity className='absolute z-20 top-[3%] left-[27%] bg-white p-1.5 rounded-full' activeOpacity={0.9}>
-                    <Ionicons name='heart-outline' size={24} color='red' />
-                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
 }
 
-export default WishlistCard
+export default OrderCard

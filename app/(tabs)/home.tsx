58d1cd4160945c6a04bd9b00628d47428ad1f9f9
Shivam Verma <mcrafter147@gmail.com>
2025-04-28 22:27:29 +0530

    Update home.tsx

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,7 +1,7 @@
 import CustomizeOwn from '@/components/home/CustomizeOwn';
 import PopularSection from '@/components/home/PopularSection';
 import { globalOffers } from '@/utils/constants/home';
-import { FlatList, ScrollView, View } from 'react-native';
+import { FlatList, Platform, ScrollView, View } from 'react-native';
 import LocationHeader from '../../components/home/LocationHeader';
 import SearchBar from '../../components/home/SearchBar';
 import SpecialOfferCard from '../../components/home/SpecialOfferCard';
@@ -25,15 +25,15 @@ const Home = () => {
   );
 
   return (
-      <FlatList
-        className="flex-1 bg-white"
-        data={[1]}
-        renderItem={() => null}
-        ListHeaderComponent={renderHeader}
-        ListFooterComponent={PopularSection}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ flexGrow: 1 }}
-      />
+    <FlatList
+      className="flex-1 bg-white"
+      data={[1]}
+      renderItem={() => null}
+      ListHeaderComponent={renderHeader}
+      ListFooterComponent={PopularSection}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ flexGrow: 1 }}
+    />
   );
 };
 
